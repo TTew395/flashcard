c71845757152df0082fea7a12a4d1cf00937e06a
TTew395 <1058771@lwsd.org>
2026-09-08 19:36:55 -0700

    Update audio.js

--- js/audio.js
+++ js/audio.js
@@ -1,14 +1,1 @@
-/**
- * Audio removed per user request.
- */
-class NoopAudio {
-  playCorrect() {}
-  playIncorrect() {}
-  playFlip() {}
-  playVictory() {}
-  playClick() {}
-  isSoundEnabled() { return false; }
-  toggleSound() { return false; }
-}
 
-const audioFX = new NoopAudio();
